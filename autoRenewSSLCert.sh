7653bab72868a2e84def0dc7a56d91e806809c5b
kevinfealey <kevin.fealey@aspectsecurity.com>
2016-12-13 09:21:32 -0500

    added file

---
+++ autoRenewSSLCert.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+HOSTNAME="my-hostname.com"
+
+if ! /letsencrypt/letsencrypt-auto renew > /var/log/letsencrypt/renew.log 2>&1 ; then
+    echo Automated renewal failed:
+    cat /var/log/letsencrypt/renew.log
+    exit 1
+fi
+ cat /var/log/letsencrypt/renew.log
+
+echo "Updating private key"
+./confdba -mk cs.priv_key --value_file=/etc/letsencrypt/live/$HOSTNAME/privkey.pem
+echo "Updating ca bundle"
+./confdba -mk cs.ca_bundle --value_file=/etc/letsencrypt/live/$HOSTNAME/fullchain.pem
+echo "Updating certificate"
+./confdba -mk cs.cert --value_file=/etc/letsencrypt/live/$HOSTNAME/cert.pem
+echo "Restarting openvpnas server"
+/etc/init.d/openvpnas restart
